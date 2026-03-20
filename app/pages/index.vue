018c2c3c9a29a2b9deb3d5017d1bc6cde53ea35d
Denki <bardey.estelle1@gmail.com>
2026-03-20 19:25:08 +0100

    Refont de la structure du code

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
+    <div class="grille">
+        <h1 class="font-outfit text-4xl font-bold text-center mt-10">
         Test
+        </h1>
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/google-fonts', '@nuxtjs/sanity', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/ui', '@nuxtjs/google-fonts', '@nuxtjs/sanity'],
+  css: ['~/assets/css/main.css'],
+  googleFonts: {
+    families: {
+      Outfit: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      'Open Sans': [300, 400, 500, 600, 700, 800]
+    },
+    display: 'swap',
+    preconnect: true,
+    download: true, 
+  }
 })
