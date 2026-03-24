26e853fa7c7bfa3883a7e05ac8a967b78349c7b7
Denki <bardey.estelle1@gmail.com>
2026-03-24 21:00:33 +0100

    Ajout d'un premier composant

--- app/components/HeroTest.vue
+++ app/components/HeroTest.vue
@@ -1,51 +1,95 @@
+<script setup>
+import { ref } from 'vue'
+const hoveredProject = ref(null)
+const getProjectStyle = (id, isBorder) => {
+    if (hoveredProject.value === null) {
+        return {
+            flex: isBorder ? 4 : 3,
+            opacity: 0.75
+        }
+    }
+    
+    if (hoveredProject.value === id) {
+        return {
+            flex: 10,
+            opacity: 1
+        }
+    }
+    
+    return {
+        flex: isBorder ? 4 : 2,
+        opacity: 0.3
+    }
+}
+</script>
 <template>
-    <section class="hero-carrousel">
-        <article class="hero__projet hero__projet_border hero__projet-1">
-            <div>
-                <div>
-                    <div>
-                        <h2>Project 1</h2>
-                        <button>Voir plus</button>
+    <section class="hero-carrousel w-[120%] h-[800px] bg-black flex -mx-[10%]" @mouseleave="hoveredProject = null">
+        <article 
+            class="hero__projet w-full h-[800px] flex justify-center items-center transform skew-x-[20deg] overflow-hidden transition-all duration-600 hero__projet-1"
+            :style="getProjectStyle(1, true)"
+            @mouseenter="hoveredProject = 1"
+        >
+            <div class="w-full h-[800px] transform -skew-x-[20deg]">
+                <div class="w-[300%] -ml-[100%] h-[800px] bg-cover bg-center bg-size-[auto] block">
+                    <div class="pt-[50px] flex flex-col justify-between h-[calc(100%-100px)] ml-[72vw]">
+                        <h2 class="text-white font-bold m-0 py-2 px-5 text-5xl w-fit z-10 bg-[#000000A0] rounded-lg ">Project 1</h2>
+                        <button class="text-white font-bold m-0 py-3 px-5 text-5xl z-10 mt-[50px] h-fit w-fit bg-[#000000A0] border-3 border-white rounded-2xl ml-[36vw]">Voir plus</button>
                     </div>
                 </div>
             </div>
         </article>
-        <article class="hero__projet hero__projet_center hero__projet-2">
-            <div>
-                <div>
-                    <div>
-                        <h2>Project 2</h2>
-                        <button>Voir plus</button>
+        <article 
+            class="hero__projet hero__projet_center w-full h-[800px] flex justify-center items-center transform skew-x-[20deg] overflow-hidden transition-all duration-600 hero__projet-2"
+            :style="getProjectStyle(2, false)"
+            @mouseenter="hoveredProject = 2"
+        >            
+            <div class="w-full h-[800px] transform -skew-x-[20deg]">
+                <div class="w-[300%] -ml-[100%] h-[800px] bg-cover bg-center bg-size-[auto] block">
+                    <div class="pt-[50px] flex flex-col justify-between h-[calc(100%-100px)] ml-[51vw]">
+                        <h2 class="text-white font-bold m-0 py-2 px-5 text-5xl w-fit z-10 bg-[#000000A0] rounded-lg">Project 2</h2>
+                        <button class="text-white font-bold m-0 py-3 px-5 text-5xl z-10 mt-[50px] h-fit w-fit bg-[#000000A0] border-3 border-white rounded-2xl ml-[46vw]">Voir plus</button>
                     </div>
                 </div>
             </div>
         </article>
-        <article class="hero__projet hero__projet_center hero__projet-3">
-            <div>
-                <div>
-                    <div>
-                        <h2>Project 3</h2>
-                        <button>Voir plus</button>
+        <article 
+            class="hero__projet hero__projet_center w-full h-[800px] flex justify-center items-center transform skew-x-[20deg] overflow-hidden transition-all duration-600 hero__projet-3"
+            :style="getProjectStyle(3, false)"
+            @mouseenter="hoveredProject = 3"
+        >
+            <div class="w-full h-[800px] transform -skew-x-[20deg]">
+                <div class="w-[300%] -ml-[100%] h-[800px] bg-cover bg-center bg-size-[auto] block">
+                    <div class="pt-[50px] flex flex-col justify-between h-[calc(100%-100px)] ml-[51vw]">
+                        <h2 class="text-white font-bold m-0 py-2 px-5 text-5xl w-fit z-10 bg-[#000000A0] rounded-lg">Project 3</h2>
+                        <button class="text-white font-bold m-0 py-3 px-5 text-5xl z-10 mt-[50px] h-fit w-fit bg-[#000000A0] border-3 border-white rounded-2xl ml-[46vw]">Voir plus</button>
+                     </div>
+                </div>
+            </div>
+        </article>
+        <article 
+            class="hero__projet hero__projet_center w-full h-[800px] flex justify-center items-center transform skew-x-[20deg] overflow-hidden transition-all duration-600 hero__projet-4"
+            :style="getProjectStyle(4, false)"
+            @mouseenter="hoveredProject = 4"
+        >
+            <div class="w-full h-[800px] transform -skew-x-[20deg]">
+                <div class="w-[300%] -ml-[100%] h-[800px] bg-cover bg-center bg-size-[auto] block">
+                    <div class="pt-[50px] flex flex-col justify-between h-[calc(100%-100px)] ml-[51vw]">
+                        <h2 class="text-white font-bold m-0 py-2 px-5 text-5xl w-fit z-10 bg-[#000000A0] rounded-lg">Project 4WDGBGGFHNGFBGH?DBGN</h2>
+                        <button class="text-white font-bold m-0 py-3 px-5 text-5xl z-10 mt-[50px] h-fit w-fit bg-[#000000A0] border-3 border-white rounded-2xl ml-[46vw]">Voir plus</button>
                     </div>
                 </div>
             </div>
         </article>
-        <article class="hero__projet hero__projet_center hero__projet-4">
-            <div>
-                <div>
-                    <div>
-                        <h2>Project 4WDGBGGFHNGFBGH?DBGN</h2>
-                        <button>Voir plus</button>
-                    </div>
-                </div>
-            </div>
-        </article>
-        <article class="hero__projet hero__projet_border hero__projet-5">
-            <div>
-                <div>
-                    <div>
-                        <h2>Project 5</h2>
-                        <button>Voir plus</button>
+        <article 
+            class="hero__projet w-full h-[800px] flex justify-center items-center transform skew-x-[20deg] overflow-hidden transition-all duration-600 hero__projet-5"
+            :style="getProjectStyle(5, true)"
+            @mouseenter="hoveredProject = 5"
+        >            
+            <div class="w-full h-[800px] transform -skew-x-[20deg]">
+                <div class="w-[300%] -ml-[100%] h-[800px] bg-cover bg-center bg-size-[auto] block">
+                    <div class="pt-[50px] flex flex-col justify-between h-[calc(100%-100px)] ml-[57vw]">
+                        <h2 class="text-white font-bold m-0 py-2 px-5 text-5xl w-fit z-10 bg-[#000000A0] rounded-lg">Project 5</h2>
+                        <button class="text-white font-bold m-0 py-3 px-5 text-5xl z-10 mt-[50px] h-fit w-fit bg-[#000000A0] border-3 border-white rounded-2xl ml-[34vw]">Voir plus</button>
                     </div>
                 </div>
             </div>
@@ -53,206 +97,25 @@
     </section>
 </template>
 <style>
-    html, body {
-        margin: 0;
-        padding: 0;
-    }
-    .hero-carrousel {
-        width: 120%;
-        height: 800px;
-        background-color: #000000;
-        display: flex;
-        margin-left: -10%;
-        margin-right: -10%;
-        
-    }
-    .hero__projet {
-        width: 100%;
-        height: 800px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        transform: skew(20deg);
-        overflow: hidden;
-        transition: all 0.6s;
-        opacity: 0.75;
-    }
-    .hero__projet-1 {
-        flex: 4;
-    }
-    /* .hero__projet-1:hover {
-        flex: 10;
-    } */
-    .hero__projet-2 {
-        flex: 3;
-    }
-    /* .hero__projet-2:hover {
-        flex: 8;
-    } */
-    .hero__projet-3 {
-        flex: 3;
-    }
-    /* .hero__projet-3:hover {
-        flex: 8;
-    } */
-    .hero__projet-4 {
-        flex: 3;
-    }
-    /* .hero__projet-4:hover {
-        flex: 8;
-    } */
-    .hero__projet-5 {
-        flex: 4;
-    }
-    .hero__projet:hover {
-        flex: 10;
-        opacity: 1;
-    }
-    .hero-carrousel:has(.hero__projet-1:hover) .hero__projet_center:not(.hero__projet-1) {
-        flex: 2;
-        opacity: 0.3;
-    }
-    .hero-carrousel:has(.hero__projet-1:hover) .hero__projet_border:not(.hero__projet-1) {
-        flex: 4;
-        opacity: 0.3;
-    }
-    .hero-carrousel:has(.hero__projet-2:hover) .hero__projet_center:not(.hero__projet-2) {
-        flex: 2;
-        opacity: 0.3;
-    }
-    .hero-carrousel:has(.hero__projet-2:hover) .hero__projet_border:not(.hero__projet-2) {
-        flex: 4;
-        opacity: 0.3;
-    }
-    .hero-carrousel:has(.hero__projet-3:hover) .hero__projet_center:not(.hero__projet-3) {
-        flex: 2;
-        opacity: 0.3;
-    }
-    .hero-carrousel:has(.hero__projet-3:hover) .hero__projet_border:not(.hero__projet-3) {
-        flex: 4;
-        opacity: 0.3;
-    }
-    .hero-carrousel:has(.hero__projet-4:hover) .hero__projet_center:not(.hero__projet-4) {
-        flex: 2;
-        opacity: 0.3;
-    }
-    .hero-carrousel:has(.hero__projet-4:hover) .hero__projet_border:not(.hero__projet-4) {
-        flex: 4;
-        opacity: 0.3;
-    }
-    .hero-carrousel:has(.hero__projet-5:hover) .hero__projet_center:not(.hero__projet-5) {
-        flex: 2;
-        opacity: 0.3;
-    }
-    .hero-carrousel:has(.hero__projet-5:hover) .hero__projet_border:not(.hero__projet-5) {
-        flex: 4;
-        opacity: 0.3;
-    }
-    .hero__projet>div {
-        width: 100%;
-        height: 800px;
-       transform: skew(-20deg);
-    }
     .hero__projet-1>div>div {
-        width: 300%;
-        margin-left: -100%;
-        height: 800px;
         background-image: url("~/assets/test/H2x1_NSwitch_BioshockRemastered_image1600w.jpg");
-        background-size: cover;
-        background-position: center;
-        background-size: auto 800px;
     }
     .hero__projet-2>div>div {
-        width: 300%;
-        margin-left: -100%;
-        height: 800px;
         background-image: url("~/assets/test/DragonQuest_XGS_HERO.jpg");
-        background-size: cover;
-        background-position: center;
-        background-size: auto 800px;
     }
     .hero__projet-3>div>div {
-        width: 300%;
-        margin-left: -100%;
-        height: 800px;
         background-image: url("~/assets/test/DANGANRONPA-V3-KILLING-HARMONY-trailer-perosnnages.webp");
-        background-size: cover;
-        background-position: center;
-        background-size: auto 800px;
     }
     .hero__projet-4>div>div {
-        width: 300%;
-        margin-left: -100%;
-        height: 800px;
         background-image: url("~/assets/test/2x1_NSwitch_Minecraft.jpg");
-        background-size: cover;
-        background-position: center;
-        background-size: auto 800px;
     }
     .hero__projet-5>div>div {
-        width: 300%;
-        margin-left: -100%;
-        height: 800px;
         background-image: url("~/assets/test/des-pitchs-pour-banjo-kazooie-seraient-ecoutes-par-microsoft-cover68684abe29f99.jpg");
-        background-size: cover;
-        background-position: center;
-        background-size: auto 800px;
-    }
-    .hero__projet>div>div{
-        display: block;
-    }
-    .hero__projet>div>div>div{
-        padding-top: 50px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        height: calc(100% - 100px);
     }
     .hero__projet>div>div>div>h2{
-        /* position: relative; */
-        color: white;
-        font-weight: bold;
-        margin: 0;
-        padding: 8px 20px;
-        font-size: 50px;
-        width: fit-content;
-        z-index: 10;
         -webkit-text-stroke: 1.2px black;
-        background-color: #000000A0;
-        border-radius: 10px;
-    }
-    .hero__projet-1>div>div>div{
-        margin-left: 72vw;
-    }
-    .hero__projet_center>div>div>div{
-        margin-left: 51vw;
-    }
-    .hero__projet-5>div>div>div{
-        margin-left: 57vw;
     }
     .hero__projet>div>div>div>button{
-        /* position: relative; */
-        color: white;
-        font-weight: bold;
-        margin: 0;
-        padding: 10px 20px;
-        font-size: 50px;
-        z-index: 10;
-        margin-top: 50px;
         -webkit-text-stroke: 1.2px black;
-        height: fit-content;
-        width: fit-content;
-        background-color: #00000080;
-        border: #ffffff 3px solid;
-        border-radius: 20px;
-    }
-    .hero__projet-1>div>div>div>button{
-        margin-left: 36vw;
-    }
-    .hero__projet_center>div>div>div>button{
-        margin-left: 46vw;
-    }
-    .hero__projet-5>div>div>div>button{
-        margin-left: 34vw;
     }
 </style>
