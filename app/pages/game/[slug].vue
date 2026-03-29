3dac53f4152f89419e65c82b52cfc2b27e64ff4e
Denki <bardey.estelle1@gmail.com>
2026-03-29 18:10:31 +0200

    Ajout des étiquettes dans l'interface

--- app/pages/game/[slug].vue
+++ app/pages/game/[slug].vue
@@ -18,6 +18,10 @@ const gamesQuery = groq`
       asset->{_id, url},
       alt
     },
+    "etiquettes": etiquettes[affichage == true]{
+      label,
+      affichage
+    }
   }
 `
 
@@ -40,11 +44,8 @@ console.log("slug: ", route.params.slug)
     <div class="absolute bottom-0 left-0 w-full h-40 flex flex-col gap-4 items-start justify-center px-12 bg-gradient-to-t from-[var(--color-Darker)] via-[#0D0D0D66] via-80% to-transparent font-outfit">
       <h1 class="text-6xl">{{ game?.title }}</h1>
       <div class="flex gap-2.5 text-xl">
-        <p class="bg-[#008800] text-[var(--color-White)] px-4 py-1 rounded-full">Test</p>
-        <p class="bg-[#008800] text-[var(--color-White)] px-4 py-1 rounded-full">Test</p>
-        <p class="bg-[#008800] text-[var(--color-White)] px-4 py-1 rounded-full">Test</p>
-        <p class="bg-[#008800] text-[var(--color-White)] px-4 py-1 rounded-full">Test</p>
-        <p class="bg-[#008800] text-[var(--color-White)] px-4 py-1 rounded-full">Test</p>      </div>
+        <p v-for="etiquette in game?.etiquettes" :key="etiquette.label" class="bg-[#008800] text-[var(--color-White)] px-4 py-1 rounded-full">{{ etiquette.label }}</p>   
+      </div>
     </div>
   </div>
 </template>
--- app/types/api/game.d.ts
+++ app/types/api/game.d.ts
@@ -1,9 +1,22 @@
-import type { SanityImageSource } from "@sanity/image-url"
+export type GameCover = {
+  _type: 'image'
+  asset: {
+    _id: string
+    url?: string
+  }
+  alt: string
+}
 
-type SanityGame = Game & {
-  _id: string;
-  priority: number;
-  hero: boolean;
-  title: string;
-  cover: SanityImageSource[];
+export type GameEtiquette = {
+  label: string
+  affichage: boolean
+}
+
+export type Game = {
+  _id: string
+  priority: number
+  hero: boolean
+  title: string
+  cover: GameCover[]
+  etiquettes: GameEtiquette[]
 }
