6237de8c6a876d97cd1c7fa4fb90d03f0decd610
Denki <bardey.estelle1@gmail.com>
2026-03-30 14:20:40 +0200

    patch du footer

--- app/components/Footer-components.vue
+++ app/components/Footer-components.vue
@@ -13,24 +13,24 @@ import LBicon from './icones/LBicon.vue';
                 <h2 class="text-4xl mb-8 shrink-0">Contact</h2>
                 <form class="flex flex-col font-outfit gap-5 flex-1">
                     <div class="flex flex-1 gap-12 shrink-0 max-h-24">
-                        <div class="flex flex-col gap-2.5 w-full text-2xl">
+                        <div class="flex flex-col gap-2.5 w-full text-xl">
                             <label for="FirstName">Prenom *</label>
                             <input type="text" id="FirstName" class="w-full bg-[var(--color-Light)] border border-[var(--color-Dark)] border-2 rounded-2xl px-4 py-1.5 h-full min-h-12 "/>
                         </div>
-                        <div class="flex flex-col gap-2.5 w-full text-2xl">
+                        <div class="flex flex-col gap-2.5 w-full text-xl">
                             <label for="LastName">Nom *</label>
                             <input type="text" id="LastName" class="w-full bg-[var(--color-Light)] border border-[var(--color-Dark)] border-2 rounded-2xl px-4 py-1.5 h-full min-h-12"/>
                         </div>
                     </div>
-                    <div class="flex flex-1 flex-col shrink-0 max-h-24 gap-2.5 w-full text-2xl">
+                    <div class="flex flex-1 flex-col shrink-0 max-h-24 gap-2.5 w-full text-xl">
                         <label for="Email">Adresse email *</label>
                         <input type="email" id="Email" class="w-full bg-[var(--color-Light)] border border-[var(--color-Dark)] border-2 rounded-2xl px-4 py-1.5 h-full min-h-12"/>
                     </div>
-                    <div class="flex flex-1 flex-col shrink-0 max-h-24 gap-2.5 w-full text-2xl">
+                    <div class="flex flex-1 flex-col shrink-0 max-h-24 gap-2.5 w-full text-xl">
                         <label for="Subject">Objet *</label>
                         <input type="text" id="Subject" class="w-full bg-[var(--color-Light)] border border-[var(--color-Dark)] border-2 rounded-2xl px-4 py-1.5 h-full min-h-12"/>
                     </div>
-                    <div class="flex flex-col flex-2 gap-2.5 max-h-64 w-full text-2xl min-h-0">
+                    <div class="flex flex-col flex-2 gap-2.5 max-h-64 w-full text-xl min-h-0">
                         <label for="Message" class="shrink-0">Message *</label>
                         <textarea id="Message" class="w-full flex-1 bg-[var(--color-Light)] border border-[var(--color-Dark)] border-2 rounded-2xl px-4 py-2 resize-none" placeholder="Votre message..."></textarea>
                     </div>
@@ -39,7 +39,7 @@ import LBicon from './icones/LBicon.vue';
             <nav class="flex flex-col col-start-8 col-span-4 gap-16">
                 <div>
                     <h2 class="text-4xl mb-8">Réseaux sociaux</h2>
-                    <ul class="flex flex-col gap-5 text-2xl">
+                    <ul class="flex flex-col gap-5 text-xl">
                         <li><a href="tel:+33632349884">+33 6 32 34 98 84</a></li>
                         <li><a href="mailto:bardey.lucas1@gmail.com">bardey.lucas1@gmail.com</a></li>
                         <li><a href="https://www.google.com/maps/place/55+rue+Charles+de+Gaulle,+90500+Beaucourt+France/@47.2345678,6.0123456,17z/data=!3m1!4b1!4m5!3m4!1s0x4789e3c1d1d1d1d1:0x1d1d1d1d1d1d1d1d!8m2!3d47.2345678!4d6.0123456">55 rue Charles de Gaulle, 90500 Beaucourt France</a></li>
@@ -47,7 +47,7 @@ import LBicon from './icones/LBicon.vue';
                 </div>
                 <div>
                     <h2 class="text-4xl mb-8">Réseaux</h2>
-                    <ul class="flex flex-col gap-5 text-2xl">
+                    <ul class="flex flex-col gap-5 text-xl">
                         <li><a href="">Linkedin</a></li>
                         <li><a href="">Instagram</a></li>
                         <li><a href="">GitHub</a></li>
