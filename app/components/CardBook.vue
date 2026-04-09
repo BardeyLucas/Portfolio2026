c3228066529275dfe577c0113d875da054ce680b
Denki <bardey.estelle1@gmail.com>
2026-04-09 15:15:57 +0200

    Mise à jour card projets

--- app/components/CardBook.vue
+++ app/components/CardBook.vue
@@ -7,13 +7,13 @@ const props = defineProps<{
 </script>
 
 <template>
-    <div class="bg-[var(--color-Dark)] rounded-[20px] overflow-hidden p-3 flex flex-col gap-3 shadow-[0_0_8px_rgba(255,255,255,0.20)]">
+    <div class="bg-[var(--color-Dark)] rounded-[24px] overflow-hidden p-3 flex flex-col gap-3 shadow-[0_0_8px_rgba(255,255,255,0.20)]">
         <div class="w-full aspect-square flex pt-8 items-end rounded-[20px] bg-cover bg-center flex-shrink-0 inset-shadow-[0_0_8px_rgba(0,0,0,0.75)]" :style="{ backgroundImage: `url(${props.book.cover?.[0]?.asset?.url})` }"/>
         <div class="w-full flex-grow flex justify-between items-center px-4 gap-4">
             <h3 class="font-outfit text-3xl">{{ props.book.tinyTitle }}</h3>
             <NuxtLink 
               :to="`/book/${props.book._id}`" 
-              class="text-white bg-[#00000099] duration-300 text-nowrap text-2xl px-3 py-1.5 rounded-lg shadow-[0_0_4px_rgba(255,255,255,0.25)] hover:shadow-[0_0_20px_rgba(255,255,255,0.5)] active:bg-[#000000] active:mt-1"
+              class="text-white bg-[#00000099] duration-300 text-nowrap text-2xl px-3 py-1.5 rounded-lg shadow-[0_0_4px_rgba(255,255,255,0.25)] hover:shadow-[0_0_20px_rgba(255,255,255,0.5)] active:bg-[#000000]"
             >
               Voir plus
             </NuxtLink>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,15 +4,15 @@ import { useSanityClient } from '~/composables/sanity'
 const client = useSanityClient()
 
 const mixedQuery = groq`
-  *[_type in ["game", "projet"]] | order(priority desc){
+  *[_type in ["game", "projet"]] | order(priority asc){
     _id,
     priority,
     hero,
     title,
     tinyTitle,
     "typeLabel": select(
+    _type == "game" => "jeu",
       _type == "projet" => "projet",
-      _type == "game" => "jeu",
       "inconnu"
     ),
     "cover": cover[]{
@@ -53,14 +53,22 @@ const { data: books } = useAsyncData('books', () => client.fetch(bookQuery))
 <template>
     <main class="overflow-hidden ">
         <Hero />
-        <section>
-            <h2 class="text-center">Projets</h2>
-            <article class="grille gap-y-10">
+        <section class="inset-shadow-[0_8px_8px_rgba(255,255,255,0.20)] pb-20 -mb-8">
+            <div class="w-fit mx-auto flex flex-col items-center py-12 gap-1">
+                <h2 class="text-center text-7xl">Projets</h2>
+                <div class="w-[calc(100%+10px)] h-1 bg-[var(--color-White)] my-4 opacity-80"/>
+            </div>
+            <article class="grille items-stretch gap-y-10">
                 <CardProjet v-for="projet in projets" :key="projet._id" :projet="projet" />
             </article>
         </section>
-        <section class="grille">
-            <h2 class="text-center col-span-12">Books</h2>
+        <section class="grille inset-shadow-[0_8px_8px_rgba(255,255,255,0.20)] rounded-t-4xl pb-12">
+            <div class="col-span-12">
+                <div class="w-fit mx-auto flex flex-col items-center py-12 gap-1">
+                    <h2 class="text-center text-7xl">Book</h2>
+                    <div class="w-[calc(100%+10px)] h-1 bg-[var(--color-White)] my-4 opacity-80"/>
+                </div>
+            </div>
             <article class="col-start-2 col-span-10 grid grid-cols-3 gap-x-12 gap-y-10">
                 <CardBook v-for="book in books" :key="book._id" :book="book" />
             </article>
