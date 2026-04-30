d59fe25dd0dde32107f14cdf03f6a37211ff9627
Denki <bardey.estelle1@gmail.com>
2026-04-30 16:17:54 +0200

    Mise à jours responive page kinado

--- app/components/Sections/conclusion.vue
+++ app/components/Sections/conclusion.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Game } from '~/types/api/game';
+import { h, type Slots } from 'vue';
 import { PortableText } from '@portabletext/vue'
 
 const props = defineProps<{
@@ -7,18 +8,24 @@ const props = defineProps<{
     color2: string
     game: Game
 }>()
+
+const portableTextComponents = {
+  block: {
+    normal: (_: unknown, { slots }: { slots: Slots }) => h('p', { class: 'text-xs lg:text-base' }, slots.default?.()),
+  },
+}
 </script>
 <template>
-    <section class="stack-section" style="--z: 12" :style="{ 'background-color': `var(${props.color1})` }">
-        <article class="stack-panel px-12 pt-36 pb-12 relative grille content-start">
-            <h2 class="text-5xl mb-12 col-span-12">Conclusion</h2>
-            <div class="col-span-7 flex flex-col gap-10 p-10 h-full" :style="{ 'background-color': `var(${props.color2})` }">
-                <h3 class="text-4xl font-outfit">Conclusion globale</h3>
-                <PortableText :value="game.conclusion?.conclusionGlobal" />
+    <section class="stack-section" style="--z: 12" :style="{  'background-color': `var(${props.color1})` }">
+        <article class="stack-panel px-5 lg:px-12 pt-12 lg:pt-36 pb-12 relative grille lg:content-start">
+            <h2 class="text-2xl lg:text-5xl mb-6 lg:mb-12 col-span-12">Conclusion</h2>
+            <div class="col-span-12 lg:col-span-7 flex flex-col gap-5 lg:gap-10 p-5 lg:p-10 h-full" :style="{ 'background-color': `var(${props.color2})` }">
+                <h3 class="text-xl lg:text-4xl font-outfit">Conclusion globale</h3>
+                <PortableText :value="game.conclusion?.conclusionGlobal" :components="portableTextComponents"/>
             </div>
-            <div class="col-span-5 flex flex-col gap-10 p-10 h-full" :style="{ 'background-color': `var(${props.color2})` }">
-                <h3 class="text-4xl font-outfit">Ce que j'en retiens</h3>
-                <PortableText :value="game.conclusion?.retenu" />
+            <div class="col-span-12 lg:col-span-5 flex flex-col gap-5 lg:gap-10 p-5 lg:p-10 lg:h-full mt-5 lg:mt-0" :style="{ 'background-color': `var(${props.color2})` }">
+                <h3 class="text-xl lg:text-4xl font-outfit">Ce que j'en retiens</h3>
+                <PortableText :value="game.conclusion?.retenu" :components="portableTextComponents"/>
             </div>
         </article>
     </section>
